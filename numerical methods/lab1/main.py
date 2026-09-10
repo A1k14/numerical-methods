e0131aff1d446c7a40548577b83f38ec84f0f309
Taras <sleeptaras@gmail.com>
2026-09-10 23:43:48 +0300

    Creating project #1

---
+++ numerical methods/lab1/main.py
@@ -0,0 +1,22 @@
+"""
+main.py
+=======
+Головна точка входу.
+  1. Метод дихотомії    (bisection.py)
+  2. Метод хорд         (chord.py)
+  3. Метод Ньютона      (newton.py)
+  4. Проста ітерація    (simple_iteration.py)
+
+
+"""
+
+EPSILON = 0.001  # Задана точність
+
+
+def main():
+
+    #1
+
+
+if __name__ == "__main__":
+    main()
